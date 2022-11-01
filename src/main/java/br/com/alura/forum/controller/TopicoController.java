2c7dea52bf0ba288fea8e6ddf2520617d3ef3f6c
Paulo Ferreira <phgferreira@outlook.com>
2022-11-01 20:04:34 -0300

    Usando classe DTO para filtrar melhor os campos que são devolvidos nas requisições

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,23 +1,25 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+// O @RestController já diz que todos os métodos vão retonar @ResponseBody
+@RestController
 @RequestMapping("topico")
 public class TopicoController {
 
     @GetMapping("lista-todos")
-    @ResponseBody
-    public List<Topico> listAll() {
-        Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-        return Arrays.asList(topico, topico, topico);
+    public List<TopicoDto> listAll() {
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+        return TopicoDto.convertList( Arrays.asList(topico, topico, topico) );
     }
 }
