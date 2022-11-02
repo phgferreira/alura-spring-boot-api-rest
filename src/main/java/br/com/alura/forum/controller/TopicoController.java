2d6800d2e70a1850e652bf6b5be3f1e0fa6a0b0b
Paulo Ferreira <phgferreira@outlook.com>
2022-11-02 11:38:41 -0300

    Configurando um retorno de erro mais amigável

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -9,6 +9,7 @@ import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
