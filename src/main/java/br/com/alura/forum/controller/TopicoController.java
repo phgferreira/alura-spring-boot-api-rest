aa9952c6a60148740a30c43fe909083c519760b2
Paulo Ferreira <phgferreira@outlook.com>
2022-11-02 10:30:46 -0300

    Recebendo um tópico por post e guardando no banco de dados

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,17 +1,15 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 // O @RestController já diz que todos os métodos vão retonar @ResponseBody
@@ -21,6 +19,15 @@ public class TopicoController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @PostMapping
+    // @RequestBody significa que os dados do TopicoForm virão pelo corpo da requisição
+    public void insert(@RequestBody TopicoForm topicoForm) {
+        topicoRepository.save( topicoForm.convert( cursoRepository ) );
+    }
 
     @GetMapping("lista-todos")
     public List<TopicoDto> listAll() {
@@ -39,4 +46,5 @@ public class TopicoController {
         List<Topico> topicos = topicoRepository.findByCurso_Nome(cursoNome);
         return TopicoDto.convertList( topicos );
     }
+
 }
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class TopicoForm {
+
+    private String titulo;
+
+    private String mensagem;
+
+    private String nomeCurso;
+
+    public Topico convert(CursoRepository cursoRepository) {
+        Topico topico = new Topico(
+                this.titulo,
+                this.mensagem,
+                cursoRepository.findByNome( this.nomeCurso ));
+        return topico;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+}
